eb362665ffbd1438bf8469c0755c0d52c7b8ac07
Godinhoweverson <>
2023-07-26 21:13:52 +0000

    reduce

---
+++ reduce.js
@@ -0,0 +1,48 @@
+  
+/**
+ * To run this file in Gitpod, use the 
+ * command node reduce.js in the terminal
+ */
+
+
+// Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => {
+    console.log(
+        "Accumulator:", acc,
+        "Current value:", curr,
+        'Total:', acc + curr
+
+    );
+    return acc + curr;
+
+},10);// It is possible define the first accumalator value
+console.log(sum);
+
+const teamMembers = [
+    {
+      name: 'Andrew',
+      profession: 'Developer',
+      yrsExperience: 5
+    },
+    {
+      name: 'Ariel',
+      profession: 'Developer',
+      yrsExperience: 7
+    },
+    {
+      name: 'Michael',
+      profession: 'Designer',
+      yrsExperience: 1
+    },
+    {
+      name: 'Kelly',
+      profession: 'Designer',
+      yrsExperience: 3
+    }
+  ];
+  
+  // Totaling a specific object property
+  
+  
+  // Grouping by a property, and totaling it too
